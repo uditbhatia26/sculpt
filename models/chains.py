a663a1c9d6a84c8bde507b48bcd79f7db4bec144
Udit26085 <bhatiaudit55@gmail.com>
2025-11-23 20:22:11 +0530

    Refactor chains.py to include ATS calculation

--- models/chains.py
+++ models/chains.py
@@ -1,17 +1,18 @@
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
-from config.prompts import res2yaml_system_prompt, jd_template
-from schema.schema import parsedJobDescription
+from config.prompts import res2yaml_system_prompt, jd_template, ats_calculation
+from schema.schema import parsedJobDescription, ATS
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 
-MODEL_NAME = "llama-3.3-70b-versatile"
-groq_llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), model=MODEL_NAME)
+MODEL_NAME = "openai/gpt-oss-120b"
+llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), model=MODEL_NAME)
 openai_llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o-mini")
 
+# Resume to YAML conversion
 prompt = ChatPromptTemplate(
         [
             ('system', res2yaml_system_prompt),
@@ -19,12 +20,22 @@ prompt = ChatPromptTemplate(
         ]
     )
 
-res2yaml_chain = prompt | groq_llm
+res2yaml_chain = prompt | llm
 
-jd_parser_llm = groq_llm.with_structured_output(parsedJobDescription)
+# Parsing the Job description
+jd_parser_llm = llm.with_structured_output(parsedJobDescription)
 jd_prompt = PromptTemplate(
     template=jd_template,
     input_variables=['job_description']
 )
 
 jd_parser_chain = jd_prompt | jd_parser_llm
+
+
+# ATS Calculation
+ats_calulcation_llm = llm.with_structured_output(ATS)
+ats_prompt = PromptTemplate(
+    template=ats_calculation,
+    input_variables=['job_title', 'skills', 'job_description', 'resume_yaml'],
+)
+ats_chain = ats_prompt | ats_calulcation_llm
